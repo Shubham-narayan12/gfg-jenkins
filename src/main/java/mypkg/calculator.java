ce0841d29c63ee7bec9dd942e83fda1593579352
shubham <shubhamnarayan45@gmail.com>
2023-09-09 15:08:42 +0530

    mul function

--- src/main/java/mypkg/calculator.java
+++ src/main/java/mypkg/calculator.java
@@ -12,12 +12,17 @@ public class calculator {
 	{
 		return a-b;
 	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
 
 
 	public static void main(String[] args) {
 		calculator ob = new calculator();
 		System.out.print("sum is : "+ob.sum(20,10));
 		System.out.print("sub is : "+ob.sub(20,10));
+		System.out.print("mul is : "+ob.mul(20,10));
 
 	}
 
